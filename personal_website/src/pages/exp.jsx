4a2c25ba2831f7356a9d0cc4f72f2ae76b16e8ad
Advait Sepuri <advaitsepuri@gmail.com>
2024-05-02 16:43:51 -0400

    vertical line

--- personal_website/src/pages/exp.jsx
+++ personal_website/src/pages/exp.jsx
@@ -4,6 +4,9 @@ function Experience() {
     return (
         <div class="exp-bg">
             <div class="exp-title">Experience</div>
+            <div class="company-containers"></div>
+            <div class="vertical-line"></div>
+            <div class="xp-desc-containers"></div>
         </div>
     );
 
