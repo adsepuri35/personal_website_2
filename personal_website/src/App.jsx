e9846ee3237fe04abf68f8b0f52466465babc708
Advait Sepuri <advaitsepuri@gmail.com>
2024-05-02 16:12:43 -0400

    Experience Background + Scrollable

--- personal_website/src/App.jsx
+++ personal_website/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './styles/App.css'
 import Home from './pages/home.jsx'
+import Experience from './pages/exp.jsx'
 
 function App() {
 
   return (
     <>
       <Home />
+      <Experience />
     </>
   )
 }
---
+++ personal_website/src/pages/exp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Experience() {
+    return (
+        <div class="exp-bg">
+
+        </div>
+    );
+
+}
+
+export default Experience
