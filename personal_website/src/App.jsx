a99f4c5e567628434211e66a133b993fbe383a4a
Advait Sepuri <advaitsepuri@gmail.com>
2024-05-02 15:34:19 -0400

    initialize vite

---
+++ personal_website/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './styles/App.css'
+import Home from './pages/home.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ personal_website/src/pages/home.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from "react";
+import '../styles/App.css'
+import backgroundImage from '../assets/website_bg.jpg';
+import profile from '../assets/pfp.jpg';
+import { FaLinkedin, FaGithub, FaMail } from 'react-icons/fa';
+
+function Home() {
+    return (
+        <div class="home-bg">
+            <div class="intro">
+                Hi, I'm <span class="name">Advait Sepuri</span>
+            </div>
+            <div class="desc">
+                I'm a computer science student at Georgia Tech focusing on AI and Systems Architecture (with a minor in math). I aim to apply my skills to fintech and software systems.
+            </div>
+            <img class="pfp" src={profile} alt="Profile"></img>
+            <div class="links">Relevant Links:</div>
+            <div class="icon-container">
+                <a href="https://www.linkedin.com/in/advait-sepuri-8239a2214/" target="_blank">
+                    <FaLinkedin className="icon"/>
+                </a>
+                <a href="https://github.com/adsepuri35" target="_blank">
+                    <FaGithub className="icon"/>
+                </a>
+                <a href="mailto: advaitsepuri@gmail.com" target="_blank">
+                    <FaMail className="icon"/>
+                </a>
+            </div>
+            
+            
+        </div>
+    );
+}
+
+export default Home;
