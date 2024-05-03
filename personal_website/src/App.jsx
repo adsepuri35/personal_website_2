e26486a8d53c6df34e2f5de4eb06a66c4f2e8a94
Advait Sepuri <advaitsepuri@gmail.com>
2024-05-03 13:47:47 -0400

    project background

--- personal_website/src/App.jsx
+++ personal_website/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './styles/App.css'
 import Home from './pages/home.jsx'
 import Experience from './pages/exp.jsx'
+import Project from './pages/project.jsx'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Home />
       <Experience />
+      <Project />
     </>
   )
 }
---
+++ personal_website/src/pages/project.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Project() {
+    return (
+        <div class="project-bg">
+            <div class="sect-title">Experience</div>
+        </div>
+    );
+}
+
+export default Project
